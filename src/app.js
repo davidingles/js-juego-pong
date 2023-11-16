39128a23080983cc8bd72fa7b91b2a9289fa765e
David Inglés <davidingleslopez@gmail.com>
2023-11-16 09:49:40 +0100

    mi primer commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+console.log('object');
+
+let time = 30;
+let movementBall = 20;
+let movementBar = 20;
+let widthScene = document.documentElement.clientWidth - 20;
+let heightScene = document.documentElement.clientHeight - 20;
+let control = false;
+
+const game = function () {
+
+
+}()
